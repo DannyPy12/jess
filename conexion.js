1230dffc9f79c6afcd83207881eb38cb96767059
DannyPy12 <nogales20104@gmail.com>
2024-07-22 08:48:28 -0700

    jess

---
+++ conexion.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: "srv1247.hstgr.io",
+  user: "u475816193_jess",
+  password: "v4P7wMJoZ#",
+  database: "u475816193_bdonlywater2",
+});
+
+pool.getConnection((err, connection) => {
+  if (!err) {
+    console.log('Conectado a la base de datos MySQL');
+    connection.release();
+  } else {
+    console.log('No está conectado, error:', err);
+  }
+});
+
+setInterval(() => {
+  pool.query('SELECT 1', (error) => {
+    if (error) {
+      console.error('Error manteniendo la conexión activa:', error);
+    }
+  });
+}, 30000); // Cada 30 segundos
+
+module.exports = pool;
